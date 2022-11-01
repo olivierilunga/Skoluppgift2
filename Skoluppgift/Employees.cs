1550eeb65b0532bb886d0549aa87e390dd0c6cbf
olivier <olivier.ilunga@hotmail.com>
2022-11-01 16:54:50 +0100

    Added employee function

---
+++ Skoluppgift/Employees.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skoluppgift
+{
+    public class Employees
+    {
+        private string _name;
+        private decimal _salary;
+
+        private readonly List<string>? _employeeNames = new List<string>();
+        private readonly List<decimal> _employeeSalaries = new List<decimal>();
+
+        public string Name { get => _name; set => _name = value; }
+        public decimal Salary { get => _salary; set => _salary = value; }
+
+
+        public Employees()
+        {
+
+        }
+        //public Employees(string name, decimal salary)
+        //{
+        //    Name = name;
+        //    Salary = salary;
+        //}
+
+        public void AddNEmployeeName(string addName)
+        {
+
+            if (addName is null)
+            {
+                throw new ArgumentNullException(nameof(addName));
+            }
+            _name = addName;
+            _employeeNames?.Add(_name);
+
+        }
+        public void AddEmployeeSalary(decimal addSalary)
+        {
+
+
+            if (addSalary < 0)
+            {
+                throw new ArgumentNullException(nameof(addSalary));
+            }
+            _salary = addSalary;
+            _employeeSalaries.Add(_salary);
+        }
+        public void GetEmployeeInformation()
+        {
+            foreach (var name in _employeeNames)
+            {
+                foreach (var salary in _employeeSalaries)
+                    Console.Write($"Employee: {name.ToLower()} has salary: {salary.ToString("C")}\n");
+
+            }
+        }
+    }
+}
---
+++ Skoluppgift/Personalregister.cs
@@ -0,0 +1,30 @@
+﻿namespace Skoluppgift
+{
+    internal class Personalregister
+    {
+        static void Main(string[] args)
+        {
+            var users = new Employees();
+            //var users2 = new Employees();
+           
+            Console.Write("Add employee: ");
+            string user1 = Console.ReadLine();
+            
+            Console.Write("Add employee salary: ");
+            decimal user2 = Convert.ToDecimal(Console.ReadLine());
+            
+            users.AddNEmployeeName(user1);
+            users.AddEmployeeSalary(user2);
+
+            users.GetEmployeeInformation();
+
+            //user2.AddNEmployeeName("nils");
+            //user2.AddEmployeeSalary(200);
+
+
+            //user.GetEmployeeInformation();
+            //user2.GetEmployeeInformation();
+
+        }
+    }
+}
