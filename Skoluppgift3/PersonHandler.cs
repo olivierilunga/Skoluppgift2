4adfd65d7b9b2b0d19512b5aca269294543d2c4b
olivier <olivier.ilunga@hotmail.com>
2022-11-09 23:47:28 +0100

    Added PrsonHandler class

--- Skoluppgift3/PersonHandler.cs
+++ Skoluppgift3/PersonHandler.cs
@@ -11,10 +11,20 @@ namespace Skoluppgift3
         public Person CreatePerson(int age, string fname, string lname, double height, double weight)
         {
             Person person = new Person();
+            return person;
+            
         }
         public void SetAge(Person pers, int age)
         {
             pers.Age = age;
+        }
+        public void SetFirstName(Person pers, string firstName)
+        {
+            pers.FName = firstName;
+        }
+        public void SetLastName(Person pers, string lastName)
+        {
+            pers.LName = lastName;
         }
     }
 }
