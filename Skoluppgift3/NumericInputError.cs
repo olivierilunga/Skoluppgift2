4aae523e88e79c9a1398c09c783e20d78b265bfd
olivier <olivier.ilunga@hotmail.com>
2022-11-10 10:57:06 +0100

    Started with Polymorfism

---
+++ Skoluppgift3/NumericInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skoluppgift3
+{
+    internal class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            throw new NotImplementedException("You tried to use a numeric only field. This fired an rror!");
+        }
+    }
+}
---
+++ Skoluppgift3/UserError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skoluppgift3
+{
+    internal abstract class UserError
+    {
+        protected UserError() { }
+
+        public abstract string UEMessage();
+         
+    }
+}
